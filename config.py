94142cbc93e341112a043a8ce83b833f1a38cce9
Baptiste68 <43000986+Baptiste68@users.noreply.github.com>
2019-04-15 16:34:46 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -6,3 +6,20 @@ import os
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+API_WIKI = {
+    'ROOT_URL' : 'https://fr.wikipedia.org/w/api.php',
+
+    'PARAM' : {
+        'action': 'query',
+        'prop': 'extracts',
+        'exintro': 1,
+        'explaintext': 1,
+        'format': 'json',
+        'indexpageids': 1,
+        'exsentences': 5,
+        'generator': 'search',
+        'gsrlimit': 1,
+    }
+}
+
