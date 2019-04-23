f916701138226f1b7e45cc51781f5c7f35d5e27b
Baptiste68 <43000986+Baptiste68@users.noreply.github.com>
2019-04-23 15:53:58 +0200

    Add files via upload

--- gbapp/static/js/question.js
+++ gbapp/static/js/question.js
@@ -11,12 +11,19 @@ function add_question(ev){
 function display_ans(json){
     const place = document.getElementById('reponses');
     var new_place = document.createElement('div');
-    var question = "Alors mon petit, ta question est donc: " + json.question;
+    var question = json.question;
     var result = json.result;
-    var link_wiki = json.link_wiki
+    var link_wiki = json.link_wiki;
+    var coord = json.coord;
+    var latt = coord['lat'];
+    var long = coord['lng'];
+    var loca = {lat: latt, lng: long};
+    var map = new google.maps.Map(document.getElementById('map'), {center: loca, zoom: 8});
+    var addr = json.candidates[0]['formatted_address'];
     var to_show = ` 
             <p> Alors mon petit, ta question est donc: ${question} </p> 
-            <p> Voici ma réponse: ${result} </p>
+            <p> Voici ma réponse: ${result} et ${addr} </p>
+            <p> ${map} </p>
             `;
     if (link_wiki != undefined){
         to_show = to_show +
